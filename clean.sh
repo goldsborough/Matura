9225a96ce49ce1e71005f72df502a917c06756fc
petergoldsborough <petergoldsborough@hotmail.com>
2015-05-19 11:57:26 +0200

    Cleaning up

---
+++ clean.sh
@@ -0,0 +1,20 @@
+f=( 'aux' 'log' 'gz' 'fdb' );
+
+count=0
+
+for i in $(find . -type f); do
+
+    if [ ! $(echo $i | egrep ".git" ) ]; then
+
+	for j in ${f[@]}; do
+
+	    if [ $(echo ${i##*.} | egrep ${j}) ]; then
+	   	echo Removing $i
+		rm $i
+		let count++
+	    fi
+	done
+    fi
+done
+
+echo Removed $count file(s)
--- englisch/plan.md
+++
@@ -1,9 +0,0 @@
-# Englisch
-
-1. Textsortenbeschreibung lesen, verstehen
-
-2. Textsorte zusammenfassen
-
-3. Schreibplan für Matura (Vorgang beim schreiben)
-
-4. Zwei Übungstexte
